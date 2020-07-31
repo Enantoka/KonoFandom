8412e80b97d1d9c04a940093b74fb2cc6527827b
Enantoka <enantoka@gmail.com>
2020-07-31 21:47:14 +1000

    Add Character and Card model.

---
+++ Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonoFandom.Models
+{
+    public class Card
+    {
+        public int CardID { get; set; }
+        
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public int Rarity { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonoFandom.Models
+{
+    public enum Weapon
+    {
+        Sword, Dagger, Rod, Staff, Spear
+    }
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        
+        [Required]
+        [StringLength(20, MinimumLength = 1)]
+        public string FirstMidName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+
+        [Required]
+        public Weapon Weapon { get; set; }
+
+        public ICollection<Card> Cards { get; set; }
+    }
+}
