fd16b4c52b3c2b1f57ca145418e37c0ee4dad6c7
Enantoka <enantoka@gmail.com>
2020-08-09 01:19:50 +1000

    Edit Login and Register views and models to support use of Usernames.

--- KonoFandom/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ KonoFandom/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel;
 
 namespace KonoFandom.Areas.Identity.Pages.Account
 {
@@ -47,6 +48,11 @@ namespace KonoFandom.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [StringLength(15, MinimumLength = 1)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +81,7 @@ namespace KonoFandom.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new KonoFandomUser { UserName = Input.Email, Email = Input.Email };
+                var user = new KonoFandomUser { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
