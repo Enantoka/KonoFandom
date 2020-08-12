bc20cf9a97a77a5f1b79f8c07357c2b72b3f858e
Enantoka <enantoka@gmail.com>
2020-08-12 23:51:45 +1000

    Fix routing and page layout issues. Edit Login post url.

--- KonoFandom/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ KonoFandom/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace KonoFandom.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Admin/Admin");
 
             if (ModelState.IsValid)
             {
