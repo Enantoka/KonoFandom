04367eb522c43da6386f1eeec626eaddc497bb9e
Enantoka <enantoka@gmail.com>
2020-11-25 00:55:16 +1100

    Add filtering of cards by element.

--- KonoFandom/wwwroot/js/site.js
+++ KonoFandom/wwwroot/js/site.js
@@ -11,16 +11,32 @@ function viewDetails(element) {
         window.location = url;
     }
 }
-$('#index tbody tr td').click(function () { viewDetails(this) });
+$('#index tbody tr td').click( function () {viewDetails(this) });
 
-$(document).ready(function () {
-    $('#sortableTable').DataTable({
-        "paging" : false,
-        "columnDefs" : [
+
+// DataTable
+var otable;
+
+$(function () {
+
+    otable = $('#sortableTable').DataTable({
+        "paging": false,
+        "columnDefs": [
             {
-                "targets": [ -2, -1 ],
+                "targets": [-2, -1],
                 "orderable": false
             }
         ]
     });
 })
+
+function myFilter() {
+    var element = $('input:checkbox[name="ele_group"]:checked').map(function () {
+        return '^' + $(this).val() + '$';
+    }).get().join('|');
+
+    otable.column(1).search(element, true, false).draw();
+    
+    
+}
+$('div.element-checkbox-filter').on('click', function () { myFilter() });
