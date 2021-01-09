8162f50d55d78a5f4c47cd8308164ec311722893
Enantoka <enantoka@gmail.com>
2021-01-09 18:25:39 +1100

    Add test database.

---
+++ KonoFandom.XUnitTest/SharedDatabaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DoomedDatabases.Postgres;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+
+namespace KonoFandom.XUnitTest
+{
+    public class SharedDatabaseFixture : IDisposable
+    {
+        private readonly ITestDatabase testDatabase;
+
+        public SharedDatabaseFixture()
+        {
+            var connectionString = "Host=localhost;Database=test;Username=postgres;Password=admin";
+            testDatabase = new TestDatabaseBuilder().WithConnectionString(connectionString).Build();
+            testDatabase.Create();
+        }
+
+        [CollectionDefinition("Database")]
+        public class DatabaseCollectionFixture : ICollectionFixture<SharedDatabaseFixture>
+        {
+
+        }
+
+        [Collection("Database")]
+        public class CreateDatabaseTests
+        {
+            [Fact]
+            public void CreateAndDropDatabase()
+            {
+                Assert.True(true);
+            }
+        }
+
+        public void Dispose()
+        {
+            testDatabase.Drop();
+        }
+    }
+}
---
+++ KonoFandom.XUnitTest/SharedDatabaseTest.cs
@@ -0,0 +1,31 @@
+﻿/*using KonoFandom.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace KonoFandom.XUnitTest
+{
+    public class SharedDatabaseTest : IClassFixture<SharedDatabaseFixture>
+    {
+        public SharedDatabaseTest(SharedDatabaseFixture fixture) => Fixture = fixture;
+
+        public SharedDatabaseFixture Fixture { get; }
+
+        [Fact]
+        public void Can_get_item()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    var controller = new CharactersController(context);
+                    var item = controller.GetCharacters();
+
+                    Assert.Single(item.Result);
+                }
+            }
+        }
+    }
+}
+*/
