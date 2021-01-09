087862016dd037fb94490b06da59f17a549cf4af
Enantoka <enantoka@gmail.com>
2021-01-09 23:37:49 +1100

    Add Integration Test for Postgresql.

--- KonoFandom.XUnitTest/SharedDatabaseFixture.cs
+++ KonoFandom.XUnitTest/SharedDatabaseFixture.cs
@@ -2,39 +2,55 @@
 using DoomedDatabases.Postgres;
 using Xunit;
 using Microsoft.Extensions.Configuration;
+using KonoFandom.Data;
+using Microsoft.EntityFrameworkCore;
+using KonoFandom.Models;
+using System.Linq;
 
 namespace KonoFandom.XUnitTest
 {
     public class SharedDatabaseFixture : IDisposable
     {
-        private readonly ITestDatabase testDatabase;
+        private readonly ITestDatabase TestDatabase;
 
         public SharedDatabaseFixture()
         {
-            var connectionString = "Host=localhost;Database=test;Username=postgres;Password=admin";
-            testDatabase = new TestDatabaseBuilder().WithConnectionString(connectionString).Build();
-            testDatabase.Create();
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            TestDatabase = new TestDatabaseBuilder().WithConfiguration(configuration).Build();
+            TestDatabase.Create();
+
+            var builder = new DbContextOptionsBuilder<KonoFandomContext>();
+            builder.UseNpgsql(TestDatabase.ConnectionString);
+            DbContext = new KonoFandomContext(builder.Options);
+            DbContext.Database.EnsureCreated();
+
+            Seed();
         }
 
-        [CollectionDefinition("Database")]
-        public class DatabaseCollectionFixture : ICollectionFixture<SharedDatabaseFixture>
-        {
+        public KonoFandomContext DbContext { get; }
 
-        }
-
-        [Collection("Database")]
-        public class CreateDatabaseTests
+        private void Seed()
         {
-            [Fact]
-            public void CreateAndDropDatabase()
-            {
-                Assert.True(true);
-            }
+            DbContext.Character.AddRange(
+                new Character
+                {
+                    Name = "Kazuma",
+                    CharacterVoice = "Fukushima Jun",
+                    Birthday = new DateTime(2020, 6, 7),
+                    Biography = "A boy reincarnated from Japan who originally was a shut-in " +
+                    "that loves games. He has the worst job - an adventurer, but with his high " +
+                    "luck stats, quick-wittedness and strength of his comrades he stands evenly " +
+                    "matched when contending against the Demon Army Generals.",
+                    IconImagePath = "https://drive.google.com/uc?export=view&id=1fImzqGmYvNJwpq1f2XdokNHttKhzLdUC",
+                    CharacterImagePath = "https://drive.google.com/uc?export=view&id=1ZPreciFaaJ_aZ8G16uL6kvrgFFyAcfZS"
+                }
+            );
+            DbContext.SaveChanges();
         }
 
         public void Dispose()
         {
-            testDatabase.Drop();
+            TestDatabase.Drop();
         }
     }
 }
--- KonoFandom.XUnitTest/SharedDatabaseTest.cs
+++ KonoFandom.XUnitTest/SharedDatabaseTest.cs
@@ -1,31 +1,67 @@
-﻿/*using KonoFandom.Controllers;
+﻿using KonoFandom.Controllers;
+using KonoFandom.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace KonoFandom.XUnitTest
 {
     public class SharedDatabaseTest : IClassFixture<SharedDatabaseFixture>
     {
-        public SharedDatabaseTest(SharedDatabaseFixture fixture) => Fixture = fixture;
-
-        public SharedDatabaseFixture Fixture { get; }
-
-        [Fact]
-        public void Can_get_item()
+        [CollectionDefinition("Database")]
+        public class DatabaseCollectionFixture : ICollectionFixture<SharedDatabaseFixture>
         {
-            using (var transaction = Fixture.Connection.BeginTransaction())
-            {
-                using (var context = Fixture.CreateContext(transaction))
-                {
-                    var controller = new CharactersController(context);
-                    var item = controller.GetCharacters();
 
-                    Assert.Single(item.Result);
-                }
+        }
+
+        [Collection("Database")]
+        public class CharacterTests
+        {
+            private SharedDatabaseFixture fixture;
+
+            public CharacterTests(SharedDatabaseFixture fixture)
+            {
+                this.fixture = fixture;
+            }
+
+            [Fact]
+            public void Get_amount_of_characters()
+            {
+                var count = fixture.DbContext.Character.Count();
+                Assert.Equal(2, count);
+            }
+
+            [Fact]
+            public void Add_characters()
+            {
+                fixture.DbContext.Character.AddRange(
+                    new Character
+                    {
+                        Name = "Test",
+                        CharacterVoice = "Test",
+                        Birthday = new DateTime(2020, 6, 7),
+                        Biography = "Test",
+                        IconImagePath = "Test",
+                        CharacterImagePath = "Test"
+                    }
+                );
+                fixture.DbContext.SaveChanges();
+                var count = fixture.DbContext.Character.Count();
+                Assert.Equal(2, count);
+            }
+        }
+
+        [Collection("Database")]
+        public class CreateDatabaseTests
+        {
+            [Fact]
+            public void CreateAndDropDatabase()
+            {
+                Assert.True(true);
             }
         }
     }
 }
-*/
