37680a4632b4e7ace5a5b86f3bae64f7cbe5b4ca
Enantoka <enantoka@gmail.com>
2021-04-01 22:07:51 +1100

    Edit default identity pages to disable irrelevant pages.

--- KonoFandom/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ KonoFandom/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -9,26 +9,9 @@ namespace KonoFandom.Areas.Identity.Pages.Account.Manage
 {
     public class PersonalDataModel : PageModel
     {
-        private readonly UserManager<KonoFandomUser> _userManager;
-        private readonly ILogger<PersonalDataModel> _logger;
-
-        public PersonalDataModel(
-            UserManager<KonoFandomUser> userManager,
-            ILogger<PersonalDataModel> logger)
+        public IActionResult OnGet()
         {
-            _userManager = userManager;
-            _logger = logger;
-        }
-
-        public async Task<IActionResult> OnGet()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            return Page();
+            return RedirectToAction("Index", "Home", new { area = "" });
         }
     }
 }
