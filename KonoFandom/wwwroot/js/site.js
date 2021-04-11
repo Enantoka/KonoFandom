64ee9ebdf5e8dc1611129af0dcbf939c57f3c885
Enantoka <enantoka@gmail.com>
2021-04-12 02:15:29 +1000

    Add jquery-ui and test drag and drop.

--- KonoFandom/wwwroot/js/site.js
+++ KonoFandom/wwwroot/js/site.js
@@ -152,8 +152,38 @@ $(function () {
     });
 })*/
 
+/*function dragover(e) {
+        e.preventDefault();
+}
+$('#div1, #skill-box').on('dragover', function () { dragover(event) });
 
-$(function () {
+function dragstart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+    //console.log($(this).attr("id"));
+}
+$('#skill-box').find("img").on('dragstart', function () { dragstart(event) });
+$('#div1').find("img").on('dragstart', function () { dragstart(event) });
+
+function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    var tgt = document.getElementById("div1");
+
+    console.log(">: " + data);
+    console.log(">>: " + e.currentTarget.firstElementChild);
+    console.log(">>>: " + document.getElementById(data));
+    if (e.target.hasChildNodes()) {
+       tgt.replaceChild(document.getElementById(data), tgt.firstElementChild);
+    } else {
+        tgt.appendChild(document.getElementById(data));
+    }
+
+    
+    //e.target.appendChild(document.getElementById(data));
+}
+$('#div1, #skill-box').on('drop', function () { drop(event) });*/
+
+/*$(function () {
     $("#div1").on("dragover", function (e) {
         e.preventDefault();
     });
@@ -165,9 +195,99 @@ $(function () {
 
     $("#div1").on("drop", function (e) {
         e.preventDefault();
-        var data = e.originalEvent.dataTransfer.getData("text");
-        e.target.appendChild(document.getElementById(data));
+*//*        var data = e.originalEvent.dataTransfer.getData("text");
 
-        $(this).off("dragover drop");
+        var test = document.getElementById(e.originalEvent.dataTransfer.getData("text"))
+
+        console.log(">>: " + e.currentTarget.firstElementChild);
+        if (e.target.hasChildNodes()) {
+            e.currentTarget.replaceChild(test, e.currentTarget.firstElementChild);
+        }
+        
+        e.target.appendChild(document.getElementById(data));*//*
+
+        var data = e.originalEvent.dataTransfer.getData("text");
+
+        console.log(">>: " + data);
+        console.log(">>: " + e.currentTarget.firstElementChild);
+        if (e.target.hasChildNodes()) {
+            e.currentTarget.removeChild(e.currentTarget.firstElementChild)
+            //e.currentTarget.replaceChild(document.getElementById(data), e.currentTarget.child);
+        }
+
+        e.target.appendChild(document.getElementById(data));
+   
     });
-})
+})*/
+
+$(function () {
+    var $skillGallery = $("#skill-gallery"),
+        $div1 = $("#div1");
+
+    // Let skill gallery and div1 items be draggable
+    $("#skill-gallery img, #div1 img").draggable({
+        revert: "invalid",
+        helper: "clone",
+        cursor: "move"
+    });
+
+    // Let div1 be droppable accepting skill gallery items
+    $div1.droppable({
+        accept: "#skill-gallery > img",
+        drop: function (event, ui) {
+            //addSkill(ui.draggable);
+            var $item = ui.draggable;
+
+            if ($(this).children().length > 0) {
+                var move = $(this).children().detach();
+                $item.parent().append(move);
+            }
+
+            $item.fadeOut(function () {
+                $item.appendTo($div1).fadeIn(function () {
+                    $item.find("img");
+                });
+            });
+
+            console.log($(this));
+            console.log($item);
+
+        }
+    });
+
+    // Let skill gallery be droppable accepting added skills from div1
+    $skillGallery.droppable({
+        accept: "#div1 > img",
+        drop: function (event, ui) {
+            removeSkill(ui.draggable);
+        }
+    });
+
+    // Add skill to div1
+    function addSkill($item) {
+
+        if ($(this).children().length > 0) {
+            var move = $(this).children().detach();
+            $item.parent().append(move);
+        }
+
+        $item.fadeOut(function () {
+            $item.appendTo($div1).fadeIn(function () {
+                $item.find("img");
+            });
+        });
+
+        console.log($(this));
+        console.log($item);
+    }
+
+    // Add skill to skill gallery
+    function removeSkill($item) {
+        $item.fadeOut(function () {
+            $item.appendTo($skillGallery).fadeIn(function () {
+                $item.find("img");
+            });
+        });
+    }
+    //console.log($(this).attr("id"));
+});
